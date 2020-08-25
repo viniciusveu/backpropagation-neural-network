002b6c394edd95f1ae5ebd040afd2f252f80db49
Vinícius Vedovotto <vinicius.vedovotto@unesp.br>
2020-08-24 22:32:18 -0300

    Camada oculta e seleção da funcao de transferencia

--- src/lib.h
+++ src/lib.h
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <locale.h>
+#include <math.h>
+#include <ctype.h>
+
 
 int Logistica(void);
 int TangHiperbolica(void);
--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 #include "lib.h"
 
 int main(int argc, char *argv[]) {
+    setlocale(LC_ALL, "Portuguese");
+
     printf(" =================================================== \n");
     printf("||  - Trabalho Prático - Inteligência Artificial   || \n");
     printf("||  - Professor Almir O. Artero                    || \n");
@@ -11,9 +13,20 @@ int main(int argc, char *argv[]) {
 
     char nome_arquivo[30];
     FILE* arquivo_treino;
+    int neur_cam_entrada;
+    int neur_cam_saida;
+    int neur_cam_oculta;
+    int i = 0;
+    char val[3];
+    char line[150];
+    char *valor;
+    char *val_ant;
+    char final;
+    char resp;
+    int op;
 
     if(argc == 1) { // Abre arquivo por parâmetro
-    
+
         printf("Você também pode entrar com o nome do arquivo direto nos argumentos. \nEx: ./RedeNeuralBP treinamento.csv \n\n");
         printf("Entre com o nome do arquivo: \n");
         scanf("%s", nome_arquivo);
@@ -26,8 +39,62 @@ int main(int argc, char *argv[]) {
         
     } else return printf("Argumentos inválidos :( \n");
 
-    if(arquivo_treino = fopen(nome_arquivo, "r+b")) printf("Foi :D \n");
-    else printf("Deu ruim :( \n");
+    if(!(arquivo_treino = fopen(nome_arquivo, "r"))) 
+        return printf("Arquivo não encontrado! :( \n");
+    
+    printf("Arquivo aberto. \n");
 
+    while (fgets(val, 4, arquivo_treino) != NULL) {    
+        i++;
+        if (val[2] != ',') {
+            neur_cam_entrada = i-1;
+            printf("Número de Neurônios na Camada de Entrada: %i \n", neur_cam_entrada);
+            fgets(val, 4, arquivo_treino);
+            break;
+        }
+    }
+
+    while (fgets(line, sizeof(line), arquivo_treino)) {
+        valor = strtok(line, ",");
+
+        while (valor != NULL) {
+            //printf("%s\n",valor);
+            val_ant = valor;
+            valor = strtok(NULL, ",");
+            
+        }
+        
+    }
+    neur_cam_saida = val_ant[0] - '0';
+    printf("Número de Neurônios na Camada de Saída: %i \n", neur_cam_saida);
+    
+    neur_cam_oculta = abs(sqrt(neur_cam_entrada * neur_cam_saida));
+    printf("Número de Neurônios na Camada Oculta: %i \n", neur_cam_oculta);
+    printf("Deseja alterar o número de neurônios na camada oculta? [S,n]: ");
+    scanf("%c", &resp);
+    resp = toupper(resp);
+    if (resp == 'S') {
+        printf("Entre com o novo número de neurônios na camada oculta: ");
+        scanf("%i", &neur_cam_oculta);
+    } else if (resp != 'N') return printf("Opção inválida :( \n");
+    printf("Número de Neurônios na Camada Oculta: %i \n", neur_cam_oculta);
+
+    printf("\nQual função de tranferência deseja utilizar? \n");
+    printf("\t 1 - Logística \n");
+    printf("\t 2 - Tangente Hiperbólica \n");
+    printf("Opção: ");
+    scanf("%i", &op);
+    printf("Q\n");
+    if (op == 1) {
+        printf("Função de Transferência escolhida: 1 - Logística \n");
+    } else if (op != 2) return printf("Opção inválida :( \n");
+    else {
+        printf("Função de Transferência escolhida: 2 - Tangente Hiperbólica \n");
+    }
+
+
+    printf("\nFinalizando o programa..... \n");
+    fclose(arquivo_treino);
+    //system("PAUSE");
     return 0;
 };
