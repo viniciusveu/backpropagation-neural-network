64a7bf8eb63f3ff23cf019786dd5aadd663d268a
Vinícius Vedovotto <vinicius.vedovotto@unesp.br>
2020-08-28 00:27:12 -0300

    Treinamento finalizado, falta ajustar a funcao de tg hiperbolica q ta dando erro de segmentacao

--- src/funcoes.c
+++ src/funcoes.c
@@ -20,18 +20,18 @@ double DerLogistica(double net);
 double DerTgHiperbolica(double net);
 void ExibeMatrizInt(int **matriz, int lin, int col);
 void ExibeMatrizDouble(double **matriz, int lin, int col);
-void Treinar(int *pesos_e, int e_tam, double **pesos_o, int neur_o, int neur_e, double **pesos_s, int lin_s, int col_s, double erro);
+double Treinar(int *valores_entrada, int tam_entrada, double **pesos_o, double **pesos_s, int neur_cam_entrada, int neur_cam_oculta, int neur_cam_saida);
 void FuncaoPropagacao(double *nets, double *propag, int quant_neur);
-void CalcularErrosSaida(double *erros_s, int desejado, double *saida, int neur_s, double *propagacao, double *net_s);
-
-
+void CalcularErrosSaida(double *erros_s, int desejado, double *saida, int neur_cam_saida, double *net_s);
+void CalcularErrosOculta(double *erros_o, double *nets_o, int neur_cam_oculta, double *erros_s, int neur_cam_saida, double **pesos_s);
+void AjustaPesosSaida(double **pesos_s, double *erros_s, double *propag_o, int neur_cam_saida, int neur_cam_oculta);
+void AjustaPesosOculta(double **pesos_o, double *erros_o, int *valor_e, int neur_cam_entrada, int neur_cam_oculta);
+double ErroRede(double *erros_saida, int neur_cam_saida);
 
 // =========================================== 
 // Funcoes
 
 double Logistica(double net) {
-    //printf("net: %lf  exp(-net):  %e  (1.0+exp(-net))  %e \n", net, exp(-net), (1+exp(-net)));
-    //printf("Result: %e \n\n", (1.0/(1.0+exp(-net))));
     return (1.0/(1.0+exp(-net)));
 }
 
@@ -49,6 +49,7 @@ double DerLogistica(double net) {
 double DerTgHiperbolica(double net) {
     return 1.0-pow(DerTgHiperbolica(net), 2.0);
 }
+
 
 void ExibeMatrizInt(int **matriz, int lin, int col) {
     for (int i=0; i<lin; i++) {
@@ -70,49 +71,59 @@ void ExibeMatrizDouble(double **matriz, int lin, int col) {
     }
 }
 
-
-void Treinar(int *valor_e, int e_tam, double **pesos_o, int neur_o, int neur_e, double **pesos_s, int neur_s, int neur_oc, double erro) {
+// matriz_amostras[linha], neur_cam_entrada+1, pesos_o, pesos_s, neur_cam_entrada, neur_cam_oculta, neur_cam_saida
+double Treinar(int *valores_entrada, int tam_entrada, double **pesos_o, double **pesos_s, int neur_cam_entrada, int neur_cam_oculta, int neur_cam_saida) {
     double net;
-    double nets_o[neur_o];
-    double nets_s[neur_s];
-    double propag_o[neur_o];
-    double saida[neur_s];
+    double nets_o[neur_cam_oculta];
+    double erros_o[neur_cam_oculta];
+    double propag_o[neur_cam_oculta];
+    double nets_s[neur_cam_saida];
+    double erros_s[neur_cam_saida];
+    double saida[neur_cam_saida];
 
-    for (int o=0; o<neur_o; o++) {
+    for (int o=0; o<neur_cam_oculta; o++) {
         net = 0;
-        for (int neur=0; neur<neur_e; neur++) {
-            net = net + (valor_e[neur] * pesos_o[o][neur]);
-            printf("%lf * %d + \n", pesos_o[o][neur], valor_e[neur]);
+        for (int neur=0; neur<neur_cam_entrada; neur++) {
+            net = net + (valores_entrada[neur] * pesos_o[o][neur]);
+            //printf("%lf * %d + \n", pesos_o[o][neur], valores_entrada[neur]);
         }
         nets_o[o] = net;
-        printf("net o = %lf\n\n", nets_o[o]);
-        //scanf("%c", &tt);
+        //printf("net o = %lf\n\n", nets_o[o]);
     }
-    FuncaoPropagacao(nets_o, propag_o, neur_o);
+    FuncaoPropagacao(nets_o, propag_o, neur_cam_oculta);
     
 
-    for (int s=0; s<neur_s; s++) {
+    for (int s=0; s<neur_cam_saida; s++) {
         net=0;
-        for (int neur=0; neur<neur_o; neur++) {
+        for (int neur=0; neur<neur_cam_oculta; neur++) {
             net = net + (propag_o[neur] * pesos_s[s][neur]);
-            printf("%lf * %f + \n", pesos_s[s][neur], propag_o[neur]);
+            //printf("%lf * %f + \n", pesos_s[s][neur], propag_o[neur]);
         }
         nets_s[s] = net;
-        printf("net s = %lf\n\n", nets_s[s]);
+        //printf("net s = %lf\n\n", nets_s[s]);
     }
-    FuncaoPropagacao(nets_s, saida, neur_s);
+    FuncaoPropagacao(nets_s, saida, neur_cam_saida);
 
     printf("Valores p/ propagação: \n");
-    for (int i=0; i<neur_o; i++) {
+    for (int i=0; i<neur_cam_oculta; i++) {
         printf("o->s: %e    |   s: %e \n", propag_o[i], saida[i]);
     }
 
-    double erros_s[neur_s];
-    CalcularErrosSaida(erros_s, valor_e[e_tam-1], saida, neur_s, propag_o, nets_s);
-    printf("Erros da camada de saida: \n");
-    for (int i=0; i<neur_s; i++) {
-        printf("Erro: %e \n", erros_s[i]);
+    CalcularErrosSaida(erros_s, valores_entrada[tam_entrada], saida, neur_cam_saida, nets_s);
+    CalcularErrosOculta(erros_o, nets_o, neur_cam_oculta, erros_s, neur_cam_saida, pesos_s);
+    printf("Erros da camada de saida e oculta: \n");
+    for (int i=0; i<neur_cam_saida && i<neur_cam_oculta; i++) {
+        printf("Erros Saida: %e | Erros Oculta: %e \n", erros_s[i], erros_o[i]);
     }
+
+    AjustaPesosSaida(pesos_s, erros_s, propag_o, neur_cam_saida, neur_cam_oculta);
+    printf("\nMatriz com os pesos da camada de saida refinados: \n");
+    ExibeMatrizDouble(pesos_s, neur_cam_saida, neur_cam_oculta);
+    AjustaPesosOculta(pesos_o, erros_o, valores_entrada, neur_cam_entrada, neur_cam_oculta);
+    printf("\nMatriz com os pesos da camada oculta refinados: \n");
+    ExibeMatrizDouble(pesos_o, neur_cam_oculta, neur_cam_entrada);
+
+    return ErroRede(erros_s, neur_cam_saida);
 }
 
 
@@ -128,19 +139,53 @@ void FuncaoPropagacao(double *nets, double *propag, int quant_neur) {
     }
 }
 
-void CalcularErrosSaida(double *erros_s, int desejado, double *saida, int neur_s, double *propagacao, double *nets_s) {
-    printf("%d\n", desejado);
-    int desejados[neur_s];
+void CalcularErrosSaida(double *erros_s, int desejado, double *saida, int neur_cam_saida, double *nets_s) {
+    //printf("%d\n", desejado);
+    int desejados[neur_cam_saida];
     desejados[desejado-1] = 1;
-    for (int i=0; i<desejado; i++) 
+    for (int i=0; i<neur_cam_saida; i++) {
         if (desejados[i] != 1) desejados[i] = 0;
-    
+        //printf(" %i | ", desejados[i]);
+    }
     if (opcoes[0] == 1) 
-        for (int i=0; i<neur_s; i++)
+        for (int i=0; i<neur_cam_saida; i++)
             erros_s[i] = (desejados[i]-saida[i]) * DerLogistica(nets_s[i]);
     else 
-        for (int i=0; i<neur_s; i++)
+        for (int i=0; i<neur_cam_saida; i++)
             erros_s[i] = (desejados[i]-saida[i]) * DerTgHiperbolica(nets_s[i]);
 }
 
+void CalcularErrosOculta(double *erros_o, double *nets_o, int neur_cam_oculta, double *erros_s, int neur_cam_saida, double **pesos_s) {
+    double soma;
+    for (int neur_o=0; neur_o<neur_cam_oculta; neur_o++) {
+        soma=0;
+        for (int neur_s=0; neur_s<neur_cam_saida; neur_s++) {
+            soma = soma + (erros_s[neur_s] * pesos_s[neur_s][neur_o]);
+        }
+        if (opcoes[0] == 1)
+            erros_o[neur_o] = soma * DerLogistica(nets_o[neur_o]);
+        else erros_o[neur_o] = soma * DerTgHiperbolica(nets_o[neur_o]);
+    }
+}
 
+void AjustaPesosSaida(double **pesos_s, double *erros_s, double *propag_o, int neur_cam_saida, int neur_cam_oculta){
+    for (int neur_s=0; neur_s<neur_cam_saida ; neur_s++) {
+        for (int neur_o=0; neur_o<neur_cam_oculta ; neur_o++) {
+            pesos_s[neur_s][neur_o] = pesos_s[neur_s][neur_o] + (erros_s[neur_s] * propag_o[neur_o]);
+        }
+    }
+}
+
+void AjustaPesosOculta(double **pesos_o, double *erros_o, int *valor_e, int neur_cam_entrada, int neur_cam_oculta) {
+    for (int neur_o=0; neur_o<neur_cam_oculta; neur_o++) {
+        for (int neur_e=0; neur_e<neur_cam_entrada; neur_e++) {
+            pesos_o[neur_o][neur_e] = pesos_o[neur_o][neur_e] + (erros_o[neur_o] * valor_e[neur_e]);
+        }
+    }
+}
+
+double ErroRede(double *erros_saida, int neur_cam_saida) {
+    double soma = 0;
+    for (int er=0; er<neur_cam_saida; er++) soma =+ pow(erros_saida[er], 2.0);
+    return soma/2.0;
+}
--- src/main.c
+++ src/main.c
@@ -3,14 +3,18 @@
 #include "funcoes.c"
 
 int main(int argc, char *argv[]) {
+
+    // Configurando ambiente
     setlocale(LC_ALL, "Portuguese");
 
+    // Apresentação do programa
     printf(" =================================================== \n");
     printf("||  - Trabalho Prático - Inteligência Artificial   || \n");
     printf("||  - Professor Almir O. Artero                    || \n");
     printf("||  - Rede Neural Backpropagation                  || \n");
     printf(" =================================================== \n");
 
+    // Declaração das variáveis
     char nome_arquivo[30];
     FILE* arquivo_treino;
     int neur_cam_entrada;
@@ -25,9 +29,14 @@ int main(int argc, char *argv[]) {
     char final;
     char resp;  
     int quant_amostras = 0;
+    int coluna = 0;
+    int linha = 0;
+    int **matriz_amostras;
+    double erro_max;
+    int num_iteracoes;
 
+    // Abrindo o Arquivo
     if(argc == 1) { // Abre arquivo por parâmetro
-
         printf("Você também pode entrar com o nome do arquivo direto nos argumentos. \nEx: ./RedeNeuralBP treinamento.csv \n\n");
         printf("Entre com o nome do arquivo: \n");
         scanf("%s", nome_arquivo);
@@ -46,6 +55,7 @@ int main(int argc, char *argv[]) {
     printf("Arquivo aberto. \n");
 
 
+    // Verificando a quantidade de deuronios na camada de entrada
     while (fgets(val, 4, arquivo_treino) != NULL) {    
         i++;
         if (val[2] != ',') {
@@ -56,11 +66,10 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    //Verificando a quantidade de neuronios na camada de saida
     while (fgets(line, sizeof(line), arquivo_treino)) {
         valor = strtok(line, ",");
-
         while (valor != NULL) {
-            //printf("%s\n",valor);
             val_ant = valor;
             valor = strtok(NULL, ",");
             
@@ -68,10 +77,11 @@ int main(int argc, char *argv[]) {
         quant_amostras++;
     }
     quant_amostras--;
-    printf("Quant amostras %d \n", quant_amostras);
+    printf("Quantidade de amostras no arquivo : %d \n", quant_amostras);
     neur_cam_saida = val_ant[0] - '0';
     printf("Número de Neurônios na Camada de Saída: %i \n", neur_cam_saida);
     
+    // Verificando a quantidade de neuronios na camada oculta
     neur_cam_oculta = abs(sqrt(neur_cam_entrada * neur_cam_saida));
     printf("Número de Neurônios na Camada Oculta: %i \n", neur_cam_oculta);
     printf("Deseja alterar o número de neurônios na camada oculta? [S,n]: ");
@@ -84,33 +94,33 @@ int main(int argc, char *argv[]) {
     printf("Número de Neurônios na Camada Oculta: %i \n", neur_cam_oculta);
 
 
-    printf("\nQual função de transferência deseja utilizar? \n");
-    printf("\t 1 - Logística \n");
-    printf("\t 2 - Tangente Hiperbólica \n");
-    printf("Opção: ");
-    scanf("%i", &opcoes[0]);
-
+    // Verificando a função de transferência desejada
+    do {
+        printf("\nQual função de transferência deseja utilizar? \n");
+        printf("\t 1 - Logística \n");
+        printf("\t 2 - Tangente Hiperbólica \n");
+        printf("Opção: ");
+        scanf("%i", &opcoes[0]);
+    } while (opcoes[0] != 1 && opcoes[0] != 2);
+    
+    // Verificando a condição de parada do treinamento
     printf("\nQual condição de parada deseja utilizar? \n");
     printf("\t 1 - Erro Máximo \n");
     printf("\t 2 - Número de Iterações \n");
     printf("Opção: ");
     scanf("%i", &opcoes[1]);
 
-
     if (opcoes[1] == 1) {
-        float erro_max;
         printf("Entre com o valor do erro máximo: \n");
-        scanf("%f", &erro_max);
+        scanf("%lf", &erro_max);
     } else if (opcoes[1] != 2) return printf("Opção inválida :( \n");
     else {
-        int num_iteracoes;
         printf("Entre com o valor máximo de iterações: \n");
         scanf("%d", &num_iteracoes);
     }
 
-    int coluna = 0;
-    int linha = 0;
-    int **matriz_amostras;
+
+    // Criando a matriz com as amostras
     matriz_amostras = (int **) malloc(quant_amostras * sizeof(sizeof(int *)));
     rewind(arquivo_treino);
     fgets(line, sizeof(line), arquivo_treino);
@@ -120,7 +130,6 @@ int main(int argc, char *argv[]) {
         valor = strtok(line, ",");
         matriz_amostras[linha] = (int *) malloc((neur_cam_entrada+1) * sizeof(int));
         while (valor != NULL && coluna <= neur_cam_entrada) {
-            //printf("%s\n",valor);
             matriz_amostras[linha][coluna] = atoi(valor);
             valor = strtok(NULL, ",");
             coluna++;
@@ -131,11 +140,12 @@ int main(int argc, char *argv[]) {
     }
     ExibeMatrizInt(matriz_amostras, quant_amostras, neur_cam_entrada+1);
 
+    // Criando as matrizes com os pesos (linhas: cada nó, coluna: valor respectivo da camada anterior)
     double **pesos_o;
     pesos_o = (double **) malloc(neur_cam_oculta * sizeof(sizeof(double *)));
     double **pesos_s;
     pesos_s = (double **) malloc(neur_cam_saida * sizeof(sizeof(double *)));
-
+    // Gerando pesos aleatórios 
     srand(time(NULL));
     for (int i=0; i<neur_cam_oculta; i++) {
         pesos_o[i] = (double *) malloc(neur_cam_entrada * sizeof(double));
@@ -148,6 +158,7 @@ int main(int argc, char *argv[]) {
             pesos_s[i][j] = (double)(rand())/(double)(RAND_MAX)*0.1;
     }
 
+    // Exibindo as matrizes com seus pesos
     puts("\n");
     printf("Pesos iniciais gerados: \n");
     printf("Camada Oculta: \n");
@@ -156,14 +167,31 @@ int main(int argc, char *argv[]) {
     printf("Camada de Saída: \n");
     ExibeMatrizDouble(pesos_s, neur_cam_saida, neur_cam_oculta);
 
-    double erro = __INT_MAX__;
+    // Loop com o treinamento
+    int contador = 0;
+    double erro;
+    int classe_posicao = neur_cam_entrada;
     for (linha=0; linha<quant_amostras; linha++) {
-        Treinar(matriz_amostras[linha], neur_cam_entrada+1, pesos_o, neur_cam_oculta, neur_cam_entrada, pesos_s, neur_cam_saida, neur_cam_oculta, erro);
-        break;
+        erro = Treinar(matriz_amostras[linha], classe_posicao, pesos_o, pesos_s, neur_cam_entrada, neur_cam_oculta, neur_cam_saida);
+        contador++;
+        if (opcoes[1] == 1) { // erro max
+            if( erro <= erro_max) {
+                printf("Erro max. atingido! Erro da rede: %e \n", erro);
+                break;
+            }
+        } else { // num iter
+            if (contador == num_iteracoes) {
+                printf("Número max. de iterações alcançado! Erro da rede: %e\n", erro);
+                break;
+            }
+        }
+        //break;
     }
+    printf("Num. de iterações realizadas: %d", contador);
 
     
-    
+
+    // Finalizando o programa
     printf("\nFinalizando o programa..... \n");
     fclose(arquivo_treino);
     return 0;
